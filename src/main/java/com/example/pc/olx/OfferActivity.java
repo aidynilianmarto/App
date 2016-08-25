a7c90b9be6259e92d7a2270423bfd70f3f45f504
Martin Andonov <martin.l.andonov@abv.bg>
2016-08-25 17:08:50 +0300

    LogIn and Offer screens

---
+++ src/main/java/com/example/pc/olx/OfferActivity.java
@@ -0,0 +1,13 @@
+package com.example.pc.olx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer);
+    }
+}
